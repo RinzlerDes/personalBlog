0fc64d58b087dcba5830e8662e63e4b0fa6bf73e
rinzler <rinzlerdes@gmail.com>
2024-10-23 17:48:04 -0400

    Added command line arguments
    cmd -addr "addr"

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,18 +1,22 @@
 package main
 
 import (
-	"fmt"
+	"flag"
 	"log"
 	"net/http"
 )
 
-const (
-	portNumber uint = 8080
-)
+type CommandLineFlags struct {
+	addr           string
+	fileServerAddr string
+}
 
 func main() {
+	var commandLineFlags CommandLineFlags
+	commandLineFlags.getCommandLineFlags()
+
 	mux := http.NewServeMux()
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	fileServer := http.FileServer(http.Dir(commandLineFlags.fileServerAddr))
 
 	mux.HandleFunc("/", homeHandler)
 	mux.HandleFunc("/posts/view", viewHandler)
@@ -23,8 +27,13 @@ func main() {
 		fileServerHandler(w, r, fileServer)
 	})
 
-	addrString := fmt.Sprintf("localhost:%d", portNumber)
+	log.Printf("Starting server on %s", commandLineFlags.addr)
+	log.Fatal(http.ListenAndServe(commandLineFlags.addr, mux))
+}
 
-	log.Printf("Starting server on :%v", portNumber)
-	log.Fatal(http.ListenAndServe(addrString, mux))
+func (flags *CommandLineFlags) getCommandLineFlags() {
+	addr := flag.String("addr", "localhost:8080", "HTTP network address")
+
+	flag.Parse()
+	flags.addr = *addr
 }
